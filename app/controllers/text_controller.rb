5c5dadd68abbdb9bd09f0ce53ca34c9e6b3e1c65
zonika <zchodosh@conncoll.edu>
2015-09-21 11:15:44 -0400

    change env variable names to specify twilio variables

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -7,8 +7,8 @@ class TextController < ApplicationController
   def send_text_message
     number_to_send_to = params[:number_to_send_to]
 
-    twilio_sid = ENV['ACCOUNT_SID']
-    twilio_token = ENV['AUTH_TOKEN']
+    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+    twilio_token = ENV['TWILIO_AUTH_TOKEN']
     twilio_phone_number = "18452131363"
 
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
