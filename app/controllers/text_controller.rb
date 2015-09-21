1ed157837e49b7e6efec7794c528f72233b2d82d
zonika <zchodosh@conncoll.edu>
2015-09-21 17:44:44 -0400

    update send_texts to use more recent syntax

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -5,23 +5,7 @@ class TextController < ApplicationController
   def index
   end
 
-  # def send_text_message
-  #   number_to_send_to = params[:number_to_send_to]
-  #
-  #   twilio_sid = ENV['TWILIO_ACCOUNT_SID']
-  #   twilio_token = ENV['TWILIO_AUTH_TOKEN']
-  #   twilio_phone_number = "18452131363"
-  #   @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-  #
-  #   @twilio_client.account.sms.messages.create(
-  #     :from => "+1#{twilio_phone_number}",
-  #     :to => number_to_send_to,
-  #     :body => "testing"
-  #   )
-  # end
-
   def send_sms
-    binding.pry
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message "HAHAHAHHAA U SUCK"
     end
