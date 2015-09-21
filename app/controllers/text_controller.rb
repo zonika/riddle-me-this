dea6bf8ffbeca60e90e6a9bd843f1945c0d74bd2
zonika <zchodosh@conncoll.edu>
2015-09-21 12:10:10 -0400

    testing text routes

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -10,7 +10,7 @@ class TextController < ApplicationController
     twilio_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_token = ENV['TWILIO_AUTH_TOKEN']
     twilio_phone_number = "18452131363"
-
+    binding.pry
     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
 
     @twilio_client.account.sms.messages.create(
@@ -18,6 +18,18 @@ class TextController < ApplicationController
       :to => number_to_send_to,
       :body => "testing"
     )
+  end
+  def get_message
+    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+    twilio_token = ENV['TWILIO_AUTH_TOKEN']
+    twilio_phone_number = "18452131363"
 
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    message = @twilio_client.account.messages.get(params[:MessageSid])
+    binding.pry
+  end
+
+  def request
+    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root "home#index"
   post '/send', to: "text#send_text_message"
+  post '/request' to: 'text#request'
   # get '/', to: "home#index"
 end
