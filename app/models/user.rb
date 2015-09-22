312101d46bd4f12c7d2818ea0b3f880cfb5f94de
MatthewKrey <kreyenglish@gmail.com>
2015-09-22 11:17:41 -0400

    everything fixed and pulled and nice again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,25 @@
 class User < ActiveRecord::Base
+  validates :phone_number, presence: true, uniqueness: true
+  validates :name, presence: true
 
   def self.send_texts
-    riddle = Riddle.all.sample.question
+    riddle = Riddle.all.sample
+    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+    twilio_token = ENV['TWILIO_AUTH_TOKEN']
+    twilio_phone_number = "18452131363"
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
     all.each do |user|
       number_to_send_to = user.phone_number
-
-      twilio_sid = ENV['TWILIO_ACCOUNT_SID']
-      twilio_token = ENV['TWILIO_AUTH_TOKEN']
-      twilio_phone_number = "18452131363"
-      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
       begin
-        message = @twilio_client.account.messages.create(
-          :body => riddle,
-          :to => "+1"+number_to_send_to.to_s,
-          :from => twilio_phone_number)
-      rescue Twilio::REST::RequestError
+        user_with_riddle = UsersRiddle.create(user_id: user.id, riddle_id: riddle.id)
+        @twilio_client.account.messages.create({
+          :from => twilio_phone_number,
+          :to => "+1" + number_to_send_to.to_s,
+          :body => riddle.question
+          })
+      rescue Twilio::REST::RequestError => error
+        puts error.message
       end
     end
   end
