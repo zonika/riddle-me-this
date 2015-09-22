8d06691d42a8f73940bf74460676530aa8232a82
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-22 17:20:42 -0500

    update send answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true
 
   def self.send_questions
-    @@for_today = Riddle.all.sample
-    riddle = @@for_today.question
+    riddle = Riddle.all.sample.question
     twilio_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_token = ENV['TWILIO_AUTH_TOKEN']
     twilio_phone_number = "18452131363"
@@ -13,7 +12,7 @@ class User < ActiveRecord::Base
     all.each do |user|
       number_to_send_to = user.phone_number
       begin
-        user_with_riddle = UsersRiddle.create(user_id: user.id, riddle_id: @@for_today.id)
+        user_with_riddle = UsersRiddle.create(user_id: user.id, riddle_id: riddle.id)
         @twilio_client.account.messages.create({
           :from => twilio_phone_number,
           :to => "+1" + number_to_send_to.to_s,
@@ -26,7 +25,7 @@ class User < ActiveRecord::Base
   end
 
   def self.send_answers
-    answer = @@for_today.answer
+    # answer = @@for_today.answer
     twilio_sid = ENV['TWILIO_ACCOUNT_SID']
     twilio_token = ENV['TWILIO_AUTH_TOKEN']
     twilio_phone_number = "18452131363"
@@ -34,6 +33,12 @@ class User < ActiveRecord::Base
 
     all.each do |user|
       number_to_send_to = user.phone_number
+      # binding.pry
+      user_riddle = UsersRiddle.where("user_id" == user.id).last
+      riddle = Riddle.find(user_riddle.riddle_id)
+      answer = riddle.answer
+      # user_riddle = UsersRiddle.where("created_at >= ?", Time.zone.now.beginning_of_day)
+      # riddle = Riddle.find(user_riddle.last.riddle_id)
       begin
         unless user.has_answered
           @twilio_client.account.messages.create({
