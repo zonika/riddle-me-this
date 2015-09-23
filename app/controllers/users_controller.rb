9114f95e4b1c25db59ca8cdc43c05db77137b6f5
zonika <zchodosh@conncoll.edu>
2015-09-23 16:52:37 -0400

    ajax funzaaaaaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(nice_params)
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      flash[:notice] = "yayayayaya"
     else
+
       render action: "index", notice: "Something went wrong. Try again."
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
