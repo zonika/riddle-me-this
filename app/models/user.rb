2f25f9c088a4a08e4ec4086790d2e423d74dbb58
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-23 16:23:20 -0500

    refactored twilio client components

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,59 +1,68 @@
 class User < ActiveRecord::Base
-  validates :phone_number, presence: true, uniqueness: true, format: { with: /\d{10}/, message: "must be a number" }
-  validates :name, presence: true
+ validates :phone_number, presence: true, uniqueness: true
+ validates :name, presence: true
 
-  def self.send_questions
-    riddle = Riddle.all.sample
-    question = riddle.question
-    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
-    twilio_token = ENV['TWILIO_AUTH_TOKEN']
-    twilio_phone_number = "18452131363"
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+ def self.twilio_client
+   @twilio_sid = ENV['TWILIO_ACCOUNT_SID']
+   @twilio_token = ENV['TWILIO_AUTH_TOKEN']
+   @twilio_phone_number = "18452131363"
+   @twilio_client = Twilio::REST::Client.new @twilio_sid, @twilio_token
+ end
 
-    all.each do |user|
-      number_to_send_to = user.phone_number
-      begin
-        user_with_riddle = UsersRiddle.create(user_id: user.id, riddle_id: riddle.id)
-        @twilio_client.account.messages.create({
-          :from => twilio_phone_number,
-          :to => "+1" + number_to_send_to.to_s,
-          :body => question
-          })
-      rescue Twilio::REST::RequestError => error
-        puts error.message
-      end
-    end
-  end
+ def self.send_questions
+   riddle = Riddle.all.sample
+   @question = riddle.question
+   twilio_client
 
-  def self.send_answers
-    twilio_sid = ENV['TWILIO_ACCOUNT_SID']
-    twilio_token = ENV['TWILIO_AUTH_TOKEN']
-    twilio_phone_number = "18452131363"
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+   all.each do |user|
+     @number_to_send_to = user.phone_number
+     begin
+       user_with_riddle = UsersRiddle.create(user_id: user.id, riddle_id: riddle.id)
+       create_twilio_client_question
+     rescue Twilio::REST::RequestError => error
+       puts error.message
+     end
+   end
+ end
 
-    all.each do |user|
-      begin
-        unless user.has_answered
-          number_to_send_to = user.phone_number
-          user_riddle = UsersRiddle.where("user_id" == user.id).last
-          riddle = Riddle.find(user_riddle.riddle_id)
-          answer = riddle.answer
-          @twilio_client.account.messages.create({
-            :from => twilio_phone_number,
-            :to => "+1" + number_to_send_to.to_s,
-            :body => answer
-            })
-        end
-      rescue Twilio::REST::RequestError => error
-        puts error.message
-      end
-    end
-    self.reset_answers
-  end
+ def self.send_answers
+   twilio_client
 
-  def self.reset_answers
-    all.each do |user|
-      user.has_answered = false
-    end
-  end
+   all.each do |user|
+     begin
+       unless user.has_answered
+         @number_to_send_to = user.phone_number
+         user_riddle = UsersRiddle.where("user_id" == user.id).last
+         riddle = Riddle.find(user_riddle.riddle_id)
+         @answer = riddle.answer
+         create_twilio_client_answer
+       end
+     rescue Twilio::REST::RequestError => error
+       puts error.message
+     end
+   end
+   self.reset_answers
+ end
+
+ def self.create_twilio_client_question
+   @twilio_client.account.messages.create({
+     :from => @twilio_phone_number,
+     :to => "+1" + @number_to_send_to.to_s,
+     :body => @question
+     })
+ end
+
+ def self.create_twilio_client_answer
+   @twilio_client.account.messages.create({
+     :from => @twilio_phone_number,
+     :to => "+1" + @number_to_send_to.to_s,
+     :body => @answer
+     })
+ end
+
+ def self.reset_answers
+   all.each do |user|
+     user.has_answered = false
+   end
+ end
 end
