17a09d557005bd51a6c44853dd9e3ec08913ea87
zonika <zchodosh@conncoll.edu>
2015-09-25 13:37:27 -0400

    prevents answering after answer has been sent out

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -3,13 +3,15 @@ class TextController < ApplicationController
 
   def send_sms
     user = User.find_by(phone_number: params["From"].gsub("+1","").to_i)
-    ans = params["Body"].downcase
-    user_riddle = UsersRiddle.where("created_at >= ?", Time.zone.now.beginning_of_day)
-    riddle = Riddle.find(user_riddle.last.riddle_id)
-    response = riddle.validate_riddle(ans,user)
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message response
+    unless user.has_answered
+      ans = params["Body"].downcase
+      user_riddle = UsersRiddle.where("created_at >= ?", Time.zone.now.beginning_of_day)
+      riddle = Riddle.find(user_riddle.last.riddle_id)
+      response = riddle.validate_riddle(ans,user)
+      twiml = Twilio::TwiML::Response.new do |r|
+        r.Message response
+      end
+      render xml: twiml.to_xml
     end
-    render xml: twiml.to_xml
   end
 end
